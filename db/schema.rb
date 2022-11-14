14e87d79913286afcad20ac95d45dd8e9374b69e
SahityaRaipura <43685621+SahityaRaipura@users.noreply.github.com>
2022-11-13 19:06:28 -0500

    reformat header

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_13_010644) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_070102) do
   create_table "meetings", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -19,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_010644) do
     t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tutor"
   end
 
   create_table "tutors", force: :cascade do |t|
