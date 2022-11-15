22620b281aa1f8cd9cfa41386537110fb359c38a
Gary <jj3179@columbia.edu>
2022-11-15 16:25:15 -0500

    blank entry prevention

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Meeting, type: :model do
   context 'validation tests' do
-
     it 'ensures title is present' do
       meeting = Meeting.new(description:'test').save
       expect(meeting).to eq(false)
