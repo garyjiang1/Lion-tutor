9b5ce0771616e15be3f7da5807dea19df69a4853
SahityaRaipura <43685621+SahityaRaipura@users.noreply.github.com>
2022-11-15 19:25:23 -0500

    more meeting cucumber tests

--- features/step_definitions/meeting_spec.rb
+++ features/step_definitions/meeting_spec.rb
@@ -1,3 +1,19 @@
+Given /the following meetings exist/ do |meetings_table|
+  meetings_table.hashes.each do |meeting|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    Meeting.create(meeting)
+  end
+end
+
+And('I am on the Tutor Profile tab page') do 
+  visit new_meeting_path
+end
+
+Then /(.*) seed meetings should exist/ do | n_seeds |
+  expect(Meeting.count).to eq n_seeds.to_i
+end
+
 When('I go to the home page') do
   visit root_path
 end
@@ -23,10 +39,41 @@ And("I should be able to fill in Start Time") do
 end
 
 And("I should be able to fill in End Time") do
-  fill_in "Start Time", with: "2022/11/16"
+  fill_in "End Time", with: "2022/11/16"
+end
+
+Then("I should be able to click Create Meeting button") do
+  click_button "Create Meeting"
+end
+
+Then("I should be able to click Meeting Changes") do
+  click_link_or_button("Meeting Changes")
+end
+
+Then("I should be able to click Delete this meeting") do
+  click_button "Delete this meeting"
+end
+
+Then("I should be able to click Back to profile") do
+  expect(page).to have_content("Back to profile")
+  visit new_meeting_path
 end
 
 
 Then("I should see a list of available meetings") do
   expect(page).to have_content("Available Meetings")
+end
+
+Then("I should see available meeting information") do
+  expect(page).to have_content("xxx") 
+end
+
+Then("I should see all available meeting information") do
+  expect(page).to have_content("xxx") 
+  expect(page).to have_content("meeting 1") 
+end
+
+Then("I should not see all available meeting information") do
+  expect(page).to_not have_content("xxx") 
+  expect(page).to_not have_content("meeting 1") 
 end
