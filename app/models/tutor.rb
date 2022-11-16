a5b9aaf13f05e46880109c5dd7d81f08226e6f1f
Gary <jj3179@columbia.edu>
2022-11-15 21:47:53 -0500

    added more RSpec tests, and fixed the uncaught bug (feedback from the TA)

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -3,7 +3,8 @@ class Tutor < ApplicationRecord
     has_many :reviews
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
     validates :phone, presence: true
+    validates_format_of :phone, :with =>  /\d[0-9]\)*\z/ , :message => "Only positive number without spaces are allowed"
     validates :bio, presence: true
 end
